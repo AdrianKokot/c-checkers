1afc93c9f0e4596a356561dd1fdeec7b6aa6f64e
Adrian Kokot <splateusz@gmail.com>
2021-04-16 14:04:15 +0200

    Add definition of board functions

--- resources/headers/board.h
+++ resources/headers/board.h
@@ -22,7 +22,14 @@ struct BoardPosition
 struct Board
 {
   sfRenderWindow *window;
+  int playerPawnCount;
+  int playerCount;
+  int boardSize;
+  int textureSize;
+  int boardBorder;
   Player **players;
+  sfTexture **tileTextures;
+  sfSprite ***tileSprites;
 };
 
 /**
@@ -40,38 +47,61 @@ Board *board_create(
     int boardSize,
     int textureSize,
     int boardBorder,
-    char boardTileTextures[2][2][255],
-    char playerPawnTextures[2][2][255]);
+    char boardTileTextures[4][255],
+    char playerPawnTextures[4][255]);
+
+/**
+ * @brief Create players for board
+ * @param board board that players will be assigned to
+ * @param intPlayerPawnTexturesCount number of pawn textures for each player
+ * @param playerPawnTextures 2d array of paths to each player standard and queen pawns textures
+ * @param intRect sfIntRect for texture
+ * @return void
+ */
+void board_createPlayers(
+    Board *board,
+    int intPlayerPawnTexturesCount,
+    char playerPawnTextures[4][255],
+    sfIntRect *intRect);
+
+/**
+ * @brief Create sprites for board tiles
+ * @param board board
+ * @return void
+ */
+void board_createBoardSprites(Board *board);
 
 /**
  * @brief Draw board tiles on sfRenderWindow
  * @return void
  */
-void board_drawBoard();
+void board_drawBoard(Board *board);
 
 /**
  * @brief Draw player pawns on sfRenderWindow
  * @return void
  */
-void board_drawPawns();
+void board_drawPawns(Board *board);
 
 /**
  * @brief Check if game has ended
  * @return true in case of win / tie, false otherwise
  */
-bool board_checkWinStatus();
+bool board_checkWinStatus(Board *board);
 
 /**
  * @brief Gets index of player that won
  * @return index of player that won, -1 if nobody won
  */
-int board_getWinStatus();
+int board_getWinStatus(Board *board);
 
 /**
  * @brief Checks if current player selected their pawn by mouse event
  *        if yes, then pawn's available moves are marked
  * @return void
  */
-void board_checkPawnSelectionByMouse();
+void board_checkPawnSelectionByMouse(Board *board);
+
+#include "./player.h"
 
 #endif
--- resources/lib/board.c
+++ resources/lib/board.c
@@ -1,1 +1,110 @@
 #include "../headers/board.h"
+
+Board *board_create(
+    sfRenderWindow *window,
+    int boardSize,
+    int textureSize,
+    int boardBorder,
+    char boardTileTextures[4][255],
+    char playerPawnTextures[4][255])
+{
+  int intBoardTilesTexturesCount = 4;
+
+  Board *board = malloc(sizeof(Board));
+
+  board->window = window;
+  board->boardSize = boardSize;
+  board->textureSize = textureSize;
+  board->boardBorder = boardBorder;
+  board->playerPawnCount = ((boardSize - 2) / 2) * (boardSize / 2);
+  board->playerCount = 2;
+
+  sfIntRect intRect = {0, 0, textureSize, textureSize};
+
+  board->tileTextures = malloc(sizeof(sfTexture *) * 4);
+  for (int i = 0; i < intBoardTilesTexturesCount; i++)
+  {
+    board->tileTextures[i] = sfTexture_createFromFile(boardTileTextures[i], &intRect);
+  }
+
+  board_createPlayers(board, 4, playerPawnTextures, &intRect);
+  board_createBoardSprites(board);
+
+  return board;
+}
+
+void board_createBoardSprites(Board *board)
+{
+  sfSprite *spriteA = createSprite_FromTexture(board->tileTextures[0]),
+           *spriteB = createSprite_FromTexture(board->tileTextures[2]);
+
+  board->tileSprites = (sfSprite ***)malloc(board->boardSize * sizeof(sfSprite **));
+
+  for (int i = 0; i < board->boardSize; i++)
+  {
+    board->tileSprites[i] = (sfSprite **)malloc(board->boardSize * sizeof(sfSprite *));
+
+    for (int j = 0; j < board->boardSize; j++)
+    {
+      sfVector2f vectorOffset = {
+          i * board->textureSize + board->boardBorder,
+          j * board->textureSize + board->boardBorder};
+
+      board->tileSprites[i][j] = (i + j) % 2 == 1
+                                     ? sfSprite_copy(spriteA)
+                                     : sfSprite_copy(spriteB);
+
+      sfSprite_move(board->tileSprites[i][j], vectorOffset);
+    }
+  }
+}
+
+void board_createPlayers(
+    Board *board,
+    int intPlayerPawnTexturesCount,
+    char playerPawnTextures[4][255],
+    sfIntRect *intRect)
+{
+  board->players = malloc(sizeof(Player) * 2);
+  for (int i = 0; i < board->playerCount; i++)
+  {
+    sfTexture **playerTextures = malloc(sizeof(sfTexture *) * intPlayerPawnTexturesCount);
+
+    for (int j = 0; j < intPlayerPawnTexturesCount; j++)
+    {
+      playerTextures[j] = sfTexture_createFromFile(playerPawnTextures[board->playerCount * i + j], intRect);
+    }
+
+    board->players[i] = player_create(board->playerPawnCount, board, playerTextures);
+  }
+}
+
+void board_drawBoard(Board *board)
+{
+  draw2dArray(board->window, board->tileSprites, board->boardSize, board->boardSize);
+}
+
+void board_drawPawns(Board *board)
+{
+  for (int i = 0; i < board->playerCount; i++)
+  {
+    for (int j = 0; j < board->players[i]->iPawnCount; j++)
+    {
+      sfRenderWindow_drawSprite(board->window, board->players[i]->pawns[j]->sprite, NULL);
+    }
+  }
+}
+
+bool board_checkWinStatus(Board *board)
+{
+  return false;
+}
+
+int board_getWinStatus(Board *board)
+{
+  return -1;
+}
+
+void board_checkPawnSelectionByMouse(Board *board)
+{
+}
